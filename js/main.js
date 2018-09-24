0ed629ee07510ce7634658d0117ce511349932ee
Darin <darin@MacBook1.local>
2018-09-23 17:56:33 -0700

    update1

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
 //  js file
+console.log("Hello Console!")
 
 function testCode() {
     // Code for localStorage/sessionStorage.
@@ -35,32 +36,67 @@ window.onload = init;
 function init() {
     var button = document.getElementById("add_button");
     button.onclick = createSticky;
-
-    for (var i = 0; i < localStorage.length; i++) {
-        var key = localStorage.key(i);
-        if (key.substring(0, 6) == "sticky") {
-            var value = localStorage.getItem(key);
-            addStickyToDOM(value);
-        }
+    var stickiesArray = getStickiesArray();
+    for (var i = 0; i < stickiesArray.length; i++) {
+        var key = stickiesArray[i];
+        var value = localStorage[key];
+        addStickyToDOM(value);
     }
 }
 
-function addStickyToDOM(value) {
+function addStickyToDOM(key, value) {
     var stickies = document.getElementById("stickies");
     var sticky = document.createElement("li");
+    sticky.setAttribute("id", key);
     var span = document.createElement("span");
     span.setAttribute("class", "sticky");
-    sticky.setAttribute("class", "flex-item");
     span.innerHTML = value;
     sticky.appendChild(span);
     stickies.appendChild(sticky);
+    sticky.onclick = deleteSticky;
 }
 
 function createSticky() {
+    var stickiesArray = getStickiesArray();
+    var currentDate = new Date();
+    var key = "sticky_" + currentDate.getTime();
     var value = document.getElementById("note_text").value;
-    var key = "sticky_" + localStorage.length;
     localStorage.setItem(key, value);
+    stickiesArray.push(key);
+    localStorage.setItem("stickiesArray", JSON.stringify(stickiesArray));
     addStickyToDOM(value);
 }
+function getStickiesArray() {
+    var stickiesArray = localStorage.getItem("stickiesArray");
+    if (!stickiesArray) {
+        stickiesArray = [];
+        localStorage.setItem("stickiesArray", JSON.stringify(stickiesArray));
+    } else {
+        stickiesArray = JSON.parse(stickiesArray);
+    }
+    return stickiesArray;
+}
 
-console.log("Hello World!")
+function deleteSticky(e) {
+    var key = e.target.id;
+    if (e.target.tagName.toLowerCase() == "span") {
+        key = e.target.parentNode.id;
+    }
+    localStorage.removeItem(key);
+    var stickiesArray = getStickiesArray();
+    if (stickiesArray) {
+        for (var i = 0; i < stickiesArray.length; i++) {
+            if (key == stickiesArray[i]) {
+                stickiesArray.splice(i,1);
+            }
+        }
+        localStorage.setItem("stickiesArray", JSON.stringify(stickiesArray));
+        removeStickyFromDOM(key);
+    }
+}
+
+function removeStickyFromDOM(key) {
+    var sticky = document.getElementById(key);
+    sticky.parentNode.removeChild(sticky);
+}
+
